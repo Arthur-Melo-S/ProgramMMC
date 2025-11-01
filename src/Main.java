1415ab7cc3546b2fed31c84b10b5893a67006ca8
Arthur Melo <arthurmelo.am94@gmail.com>
2025-11-01 15:57:33 -0300

    chore: ajuste no print da tela inicial

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("============ CALCULO DO MMC ===========");
-        System.out.println("=======================================");
+        System.out.println("|=========== CALCULO DO MMC ==========|");
+        System.out.println("|=====================================|");
         System.out.println("Digite o primeiro numero:");
         int numero1 = sc.nextInt();
         System.out.println("Digite o segundo numero:");
